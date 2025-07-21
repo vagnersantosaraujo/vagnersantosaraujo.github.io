f5e73672d12a4520d80b9aebd2d701897976b5ea
VagnerSantosAraujo <vagnersantosaraujo@gmail.com>
2025-07-20 23:09:20 -0300

    Adição do favicon e Posts na index

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// 1. Seleciona o elemento no HTML que tem o id "posts-container".
+//    É aqui que vamos colocar todos os nossos posts.
+const postsContainer = document.getElementById('posts-container');
 
-// Apenas para testar se nosso script está vendo os dados do outro arquivo.
-console.log(posts);
+// 2. Cria um "loop" que passa por cada item (cada 'post') dentro do array 'posts'.
+posts.forEach(post => {
+  
+// 3. Para cada post, cria um elemento <article> no HTML.
+  const articleElement = document.createElement('article');
+  
+// 4. Preenche o conteúdo do <article> com os dados do post.
+//    Estamos usando "template literals" (o texto entre crases ``)
+//    para montar o HTML de forma mais fácil.
+  articleElement.innerHTML = `
+    <h2>${post.title}</h2>
+    <p><em>Por ${post.author} em ${post.date}</em></p>
+    <div>${post.content}</div>
+  `;
+  
+// 5. Adiciona o novo <article> que acabamos de criar dentro do 'postsContainer'.
+  postsContainer.appendChild(articleElement);
+});
